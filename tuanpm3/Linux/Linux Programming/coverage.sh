6b42d9b8a5723947dcaecf7c229d26c5ca3e6c50
caodoc <72246940+caodoc@users.noreply.github.com>
2024-10-29 15:17:02 +0700

    .

--- tuanpm3/Linux/Linux Programming/coverage.sh
+++
@@ -1,3 +0,0 @@
-gcc -o main main.c dummy_func.c cui_showswlaparam.c -fmax-errors=10 -w -lpthread -DLINUX -fprofile-arcs -ftest-coverage
-lcov --capture --directory ./ --output-file coverage.info
-genhtml coverage.info --output-directory output_coverage
